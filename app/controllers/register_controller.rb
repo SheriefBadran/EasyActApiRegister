2c2dc1509bb79a87cca838d449736f680035c6a3
SheriefBadran <sb222rf@student.lnu.se>
2015-01-22 16:33:09 +0100

    model printed to view

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,6 +1,10 @@
 class RegisterController < ApplicationController
 
   def index
+    @user = User.new
+    @user.app_name = 'testApp4'
+    @user.email = 'test@test3.com'
+    @user.save
     @user = User.find(1)
   end
 end
